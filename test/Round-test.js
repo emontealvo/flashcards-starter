dd7a1235980ac98d210fb339d3404b2ff25bc9ce
emontealvo <edwin.montealvo@gmail.com>
2020-05-05 22:21:59 -0600

    Add round class constructor

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Card = require('../src/Card');
-const Deck = require('../src/Deck');
+// const Card = require('../src/Card');
+// const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 it('should be a function', function() {
