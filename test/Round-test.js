46e2ded0a3e2c7e87bb22f079d908dd6f36c5b50
emontealvo <edwin.montealvo@gmail.com>
2020-05-06 14:15:21 -0600

    Add one test to check for the existence of a deck instance inside the round class

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-// const Card = require('../src/Card');
-// const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 describe('Round', function () {
@@ -12,20 +12,38 @@ describe('Round', function () {
     expect(Round).to.be.a('function');
   });
 
-  it('should be an instance of Deck', function() {
+  it('should be an instance of Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   });
 
+  // - [ ] currentCard should be the first Card in the Deck at the start of the Round
+            // I should take the first card instance within our cardSet inside the Deck class and set it to it's own property
+            // of currentCatd 
+            // This can still be broken down further...as it's making many assumptions
+            // first each round should include a deck of cards 
+            // this means to check for a deck property and have the deck be an instance of the Deck class 
+            // check for sad paths there (when deck is not an instance of Deck class what happens?) 
+            // then we must assert that that there is a currentCard property and 
+            // (separate test) it's value is an instanceOf Card 
 
+
+  it('should be initiated with a deck', function() {
+
+    const card1 = new Card (1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    const card2 = new Card (2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+    const card3 = new Card (3, "What does the callback function for find() return?", ["boolean", "array", "object"], "boolean")
+    const card4 = new Card (4, "Which iteration method returns an array of the same length as the original array?", ["map()", "forEach()", "reduce()"], "map()")
+    const deck = new Deck ([card1, card2, card3, card4]);
+    const round = new Round(deck);
+
+    expect(round.deckAtHand).to.deep.equal(deck)
+  });
 });
 
 
 // Round Class:
 
-// - [ ] currentCard should be the first Card in the Deck at the start of the Round
-          // I should take the first card instance within our cardSet inside the Deck class and set it to it's own property
-          // of currentCatd 
 
 // - [ ] returnCurrentCard: a methods that return the current card being played
         // Simple enough a method that return the currentCard at play, it may require to evenutally take the Turn class method of returnClass to 
