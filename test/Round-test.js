fb9c490e0066c1b3a681732289ad9f5de9ecd7c4
emontealvo <edwin.montealvo@gmail.com>
2020-05-05 22:08:08 -0600

    Add basic test to check for round class existence

---
+++ test/Round-test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
+it('should be a function', function() {
+  const round = new Round();
+  expect(Round).to.be.a('function');
+});
+
+it('should be an instance of Deck', function() {
+  const round = new Round();
+  expect(round).to.be.an.instanceof(Round);
+});
