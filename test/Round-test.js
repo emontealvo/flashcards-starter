6d0170460da0624327a3f79dc9d86e24620b5027
emontealvo <edwin.montealvo@gmail.com>
2020-05-07 16:10:14 -0600

    Add test to check for consecutive card change

--- test/Round-test.js
+++ test/Round-test.js
@@ -54,7 +54,7 @@ describe('Round', function () {
     const deck = new Deck ([card1, card2, card3, card4]);
     const round = new Round(deck);
 
-    expect(round.cardAtPlay).to.deep.equal(deck.cardSet[0])
+    expect(round.cardAtPlay).to.deep.equal(round.deckAtHand.cardSet[0])
   });
 
   it('should be an empty object if the deck has no cards', function() {
@@ -123,18 +123,53 @@ describe('Round', function () {
 
       expect(round.turnCount).to.equal(3)
     });
+
+    it('should change cards after a turn is taken', function() {
+
+      const card1 = new Card (1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+      const card2 = new Card (2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+      const card3 = new Card (3, "What does the callback function for find() return?", ["boolean", "array", "object"], "boolean")
+      const card4 = new Card (4, "Which iteration method returns an array of the same length as the original array?", ["map()", "forEach()", "reduce()"], "map()")
+      const deck = new Deck ([card1, card2, card3, card4]);
+      const round = new Round(deck);
+
+      round.takeTurn()
+
+      expect(round.cardAtPlay).not.to.equal(round.deckAtHand.cardSet[0])
+    });
+
+    it('should change to the second card after the first turn is taken', function() {
+
+      const card1 = new Card (1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+      const card2 = new Card (2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+      const card3 = new Card (3, "What does the callback function for find() return?", ["boolean", "array", "object"], "boolean")
+      const card4 = new Card (4, "Which iteration method returns an array of the same length as the original array?", ["map()", "forEach()", "reduce()"], "map()")
+      const deck = new Deck ([card1, card2, card3, card4]);
+      const round = new Round(deck);
+
+      round.takeTurn();
+
+      expect(round.cardAtPlay).to.equal(round.deckAtHand.cardSet[1])
+    });
+
+    it('should change cards in consecutive order', function() {
+
+      const card1 = new Card (1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+      const card2 = new Card (2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+      const card3 = new Card (3, "What does the callback function for find() return?", ["boolean", "array", "object"], "boolean")
+      const card4 = new Card (4, "Which iteration method returns an array of the same length as the original array?", ["map()", "forEach()", "reduce()"], "map()")
+      const deck = new Deck ([card1, card2, card3, card4]);
+      const round = new Round(deck);
+
+      round.takeTurn()
+      round.takeTurn()
+      round.takeTurn()
+
+      expect(round.cardAtPlay).to.equal(round.deckAtHand.cardSet[3])
+    })
   })
   // - [ ] takeTurn: a method that holds a bunch of other methods:
  
-  
-  // updates turns count,
-  // - - [ ] turn count is updated, regardless of wether guess is correct or not
-    // First we check that each round has default turn count of 0 
-    // Each time a turn instance is created a counter keeping track of the number of turns taken is updated
-    // this happens wheter the guess is correct or not, should be no problem if function is seperate from the check function. 
-    // I see three test default value, one turn and multiple turns 
-
-  
   // - - [ ] When a guess is made, a new Turn instance is created.
     // test:
     // it should instantiate a new Turn with user guess and the round's current card
